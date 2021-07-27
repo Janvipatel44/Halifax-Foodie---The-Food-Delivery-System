1c72bdd841f186cf5494afed98019042f65a94ab
janvi <jn410076@dal.ca>
2021-07-27 00:07:31 -0300

    code modified..

--- src/Services/Public/recipeSimilarity.service.js
+++ src/Services/Public/recipeSimilarity.service.js
@@ -2,11 +2,10 @@ import axios from 'axios';
 
 const ReceipeSimilarity= (request)=>{
     console.log('new:',request);
-    let config = {
-        headers: {"Access-Control-Allow-Origin": "*"}
-    }
-    let data = {"text": "bread, butter, tomato, onion, coriander chutney"};
-    return axios.post("https://us-central1-csci5410project.cloudfunctions.net/cloudFunction_ReceipeSimilarity", data,config).then(response => {
+
+    let data = {};
+    data ['text'] = "bread, butter, tomato, onion, coriander chutney";
+    return axios.post("https://us-central1-csci5410project.cloudfunctions.net/cloudFunction_ReceipeSimilarity/", data).then(response => {
         console.log(response);
         response.send(response);
     })
