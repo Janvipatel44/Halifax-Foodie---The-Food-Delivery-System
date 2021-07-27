c111c1fe47ae67a10695727f385ed642ed5a7215
janvi <jn410076@dal.ca>
2021-07-27 00:32:05 -0300

    receipe similarity

--- src/Services/Public/recipeSimilarity.service.js
+++ src/Services/Public/recipeSimilarity.service.js
@@ -2,10 +2,12 @@ import axios from 'axios';
 
 const ReceipeSimilarity= (request)=>{
     console.log('new:',request);
-
+        
     let data = {};
     data ['text'] = "bread, butter, tomato, onion, coriander chutney";
-    return axios.post("https://us-central1-csci5410project.cloudfunctions.net/cloudFunction_ReceipeSimilarity/", data).then(response => {
+    const url = 'https://us-central1-csci5410project.cloudfunctions.net/cloudFunction_ReceipeSimilarity/?'+'text'+'='+data['text']
+
+    return axios.post(url, data).then(response => {
         console.log(response);
         response.send(response);
     })
