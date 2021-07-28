b13a35ca204f9cba9782ea494e77e7e0859f256d
janvi <jn410076@dal.ca>
2021-07-28 04:22:57 -0300

    receipe Similarity

--- src/components/Private/WordCloud/WordCloud.js
+++ src/components/Private/WordCloud/WordCloud.js
@@ -7,47 +7,55 @@ const WordCloud = () => {
   const [ wordCloud , setWordCloud] = useState();
   const [ value , setValue] = useState();
 
+  const sendDataForValue = async function(response) {
+  } 
+
 useEffect(() =>{
   
   axios.post("https://cvyb3ge2j4.execute-api.us-east-1.amazonaws.com/default/wordCloudGetDataGroup11",JSON.stringify({data: review})).then((response) => {
-      let row = ""
+    let row = "";
+    response.data.Items.forEach((element, index) => {
+      row = row + element.message;
+      row = row + " ";
+  });
+  setValue(row);
       console.log(response);
-      response.data.Items.forEach((element, index) => {
-          row = row + element.message;
-          row = row + " ";
-      });
-      setValue(row);
       console.log('value',value);
       alert('Successfully stored data into database');
-      axios.request(
-        {
-        method: 'POST',
-        url: 'https://textvis-word-cloud-v1.p.rapidapi.com/v1/textToCloud',
-        headers: {
-            'content-type': 'application/json',
-            'x-rapidapi-key': 'f870c70cccmsh264557ff26f7acep1588fdjsn9d67394fc992',
-            'x-rapidapi-host': 'textvis-word-cloud-v1.p.rapidapi.com'
-        },
-        data: {
-          text: value,
-          scale: 0.5,
-          width: 400,
-          height: 400,
-          colors: ['#375E97', '#FB6542', '#FFBB00', '#3F681C'],
-          font: 'Tahoma',
-          use_stopwords: true,
-          language: 'en',
-          uppercase: false
-        }
-    }).then((response) => {
-        setWordCloud(response.data);
-    }).catch((error) => {
-        console.log("Eroor")
-    })
+  
   }).catch((error) => {
       console.log("Eroor")
   })
 } , [])
+
+useEffect(() =>{
+  axios.request(
+    {
+    method: 'POST',
+    url: 'https://textvis-word-cloud-v1.p.rapidapi.com/v1/textToCloud',
+    headers: {
+        'content-type': 'application/json',
+        'x-rapidapi-key': 'f870c70cccmsh264557ff26f7acep1588fdjsn9d67394fc992',
+        'x-rapidapi-host': 'textvis-word-cloud-v1.p.rapidapi.com'
+    },
+    data: {
+      text: value,
+      scale: 0.5,
+      width: 400,
+      height: 400,
+      colors: ['#375E97', '#FB6542', '#FFBB00', '#3F681C'],
+      font: 'Tahoma',
+      use_stopwords: true,
+      language: 'en',
+      uppercase: false
+    }
+}).then((response) => {
+    setWordCloud(response.data);
+}).catch((error) => {
+    console.log("Eroor")
+})
+
+} , [value])
 
    
   return(   <div className="row justify-content-center" >
